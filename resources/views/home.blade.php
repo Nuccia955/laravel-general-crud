063f184ec0e9e3b888aa85a465f0242339ef7b11
Nuccia955 <elenamunaro1995@gmail.com>
2022-01-29 11:43:24 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+{{-- HOMEPAGE --}}
+@extends('layouts.main')
+
+@section('main-content')
+    <section class="container welcome">
+        <h1>
+            Welcome in jokes DB
+        </h1>
+    </section>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous'/>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css' integrity='sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==' crossorigin='anonymous'/>
+    <title>Jokes</title>
+</head>
+<body>
+    @include('partials.header')
+
+    @yield('main-content')
+
+    @include('partials.footer')
+</body>
+</html>
